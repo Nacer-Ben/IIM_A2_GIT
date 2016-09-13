4c7a13e3ba3b733e3f2ec0fb17a6b016895d3d5a
Benhammouda <nacer-eddine.benhammouda@devinci.fr>
2016-09-13 16:29:07 +0200

    fix(add-music): Correction du cryptage dans la BDD

--- add_music.php
+++ add_music.php
@@ -7,16 +7,15 @@ if( isset($_FILES['music']) && !empty($_FILES['music']) &&
 	isset($_POST['title']) && !empty($_POST['title'])){
 
 	$file = $_FILES['music'];
-
 	// Si le "fichier" reçu est bien un fichier
 		$ext = strtolower(substr(strrchr($file['name'], '.')  ,1));
 		// Vérification des extentions
 		if (preg_match('/\.(mp3|ogg)$/i', $file['name'])) {
-			$filename = md5(uniqid(rand()== true));
+			$filename = md5(uniqid(rand(), true));
 			$destination = "musics/{$filename}.{$_SESSION['id']}.{$ext}";
 
 			// TODO
-			if (addMusic($db, $_SESSION['id'], $_POST['title'], $_FILES['music']) == true) {
+			if (addMusic($db, $_SESSION['id'], $_POST['title'], $destination) == true) {
 				header('Location:dashboard.php');
 			}
 
@@ -24,8 +23,6 @@ if( isset($_FILES['music']) && !empty($_FILES['music']) &&
 			$error = 'Erreur, le fichier n\'a pas une extension autorisée !';
 		}
 	// }
-} else {
-	echo "error";
 }
 
 include 'view/_header.php';
